6259a7f4eb6830e6681e77d0412af3ca3d1832ce
Kilik <bestkilik@gmail.com>
2021-01-23 17:33:02 +0800

    nRF24測試代碼

--- NRF24_Send/NRF24_Send.ino
+++ NRF24_Send/NRF24_Send.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <RF24.h>
+#include <RF24_config.h>
+#include <printf.h>
+#include <nRF24L01.h>
 
+RF24 radio(7,8);
+const byte address[6] = "00101";
+
+int count = 0;
+
+void setup() {
+	Serial.begin(9600);
+	radio.begin();					//初始化 nRF24L01摸組
+	radio.openWritingPipe(address);	//開始寫入管線
+	radio.setPALevel(RF24_PA_MIN);	//設為低功率
+	radio.stopListening();			//傳送端不需接收
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	char text[32];
+	sprintf(text, "Hello World %d", count);
+	Serial.println(text);
 
+	radio.write(&text, sizeof(text));
+	count++;
+	delay(1000);
 }
