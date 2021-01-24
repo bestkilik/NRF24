cabb4a5bf93d7eaece883b67579fc3b550a1c820
Kilik <bestkilik@gmail.com>
2021-01-24 10:13:53 +0800

    測試接收

--- NRF24_Receive/NRF24_Receive.ino
+++ NRF24_Receive/NRF24_Receive.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <RF24_config.h>
+#include <RF24.h>
+#include <printf.h>
+#include <nRF24L01.h>
 
+RF24 radio(7,8);
+const byte address[6] = "00001";
+
+void setup() {
+	Serial.begin(9600);
+	printf_begin();
+	radio.openReadingPipe(0, address);
+	radio.setPALevel(RF24_PA_MIN);
+	radio.startListening();
+	radio.printDetails();
+	Serial.println("NRF2401 receiver");
+	Serial.println("waiting...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+	if (radio.available()) {
+		char text[32] = "";
+		radio.read(&text, sizeof(text));
+		Serial.println(text);
+	}
 }
