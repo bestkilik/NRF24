47d9230a0d14b38f76a678bd38b76bc72ab28706
bestkilik <bestkilik@gmail.com>
2022-05-05 17:08:25 +0800

    Update NRF24_Send.ino

--- NRF24_Send/NRF24_Send.ino
+++ NRF24_Send/NRF24_Send.ino
@@ -3,24 +3,24 @@
 #include <printf.h>
 #include <nRF24L01.h>
 
-RF24 radio(7,8);
+RF24 radio(7, 8);
 const byte address[6] = "00101";
 
 int count = 0;
 
 void setup() {
 	Serial.begin(9600);
-	radio.begin();					//初始化 nRF24L01摸組
-	radio.openWritingPipe(address);	//開始寫入管線
-	radio.setPALevel(RF24_PA_MAX);	//設為低功率
-	radio.stopListening();			//傳送端不需接收
-//  radio.printDetails();
+	radio.begin();                  //初始化 nRF24L01摸組
+	radio.openWritingPipe(address); //開始寫入管線
+	radio.setPALevel(RF24_PA_MAX);  //設為低功率
+	radio.stopListening();          //傳送端不需接收
+	// radio.printDetails();
 }
 
 void loop() {
 	char text[32];
 	sprintf(text, "Hello World %d", count);
-//	Serial.println(text);
+	// Serial.println(text);
 
 	radio.write(&text, sizeof(text));
 	count++;
