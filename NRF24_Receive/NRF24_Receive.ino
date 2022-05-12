dff838cb8184fed676a84f41aa7b6dbfe2b4c7ac
Kilik <bestkilik@gmail.com>
2022-05-12 12:21:41 +0800

    日常測試

--- NRF24_Receive/NRF24_Receive.ino
+++ NRF24_Receive/NRF24_Receive.ino
@@ -6,42 +6,58 @@
 unsigned long appTime;
 unsigned long timer = 0;
 RF24 radio(7, 8);
-const byte address[6] = "00101";
-
+const byte address[][6] = {"QWE","ASD"};
+const byte address0[6] = "00005";
 void setup() {
-	Serial.begin(9600);
-	radio.begin();
-	printf_begin();
-	radio.openReadingPipe(0, address);
-	
-	radio.setPALevel(RF24_PA_MAX);
-	radio.startListening();
-	radio.printDetails();
-	Serial.println("NRF2401 receiver");
-	Serial.println("waiting...");
-	appTime = millis();
+//	Serial.begin(115200);
+//	radio.begin();
+//	printf_begin();
+//	radio.setPALevel(RF24_PA_MAX);
+//	
+//	radio.openWritingPipe(address[1]);
+//	radio.openReadingPipe(0, address[0]);
+//	
+//	
+//	radio.startListening();
+//	radio.printDetails();
+//	Serial.println("NRF2401 receiver");
+//	Serial.println("waiting...");
+//	appTime = millis();
+//
+//	pinMode(LED_BUILTIN, OUTPUT);
 
-	pinMode(LED_BUILTIN, OUTPUT);
+	Serial.begin(115200);
+    radio.begin();
+    printf_begin();
+    // Set the PA Level low to prevent power supply related issues since this is a
+    // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+    radio.setPALevel(RF24_PA_LOW);
+    
+    // Open a writing and reading pipe on each radio, with opposite addresses
+//    radio.openWritingPipe(address0);
+    radio.openReadingPipe(1,address[0]);
+    radio.printDetails();
+    radio.startListening();
 }
 
 void loop() {
-	unsigned long dt = millis() - appTime;
-	appTime = millis();
-	if (timer > 0){
-		timer -= dt;
-	}
-  
-	if (timer <= 0) {
-		timer = 0;
-	}
-
-	uint8_t pipe;
-	if (radio.available(&pipe)) {
-		char text[32] = "";
-		radio.read(&text, sizeof(text));
-		timer = 1.0;
-		Serial.println(text);
-		Serial.print(F(" bytes on pipe "));
-		Serial.print(pipe);
-	}
+//	unsigned long dt = millis() - appTime;
+//	appTime = millis();
+//	if (timer > 0){
+//		timer -= dt;
+//	}
+//  
+//	if (timer <= 0) {
+//		timer = 0;
+//	}
+//
+//	uint8_t pipe;
+//	if (radio.available(&pipe)) {
+//		char text[32] = "";
+//		radio.read(&text, sizeof(text));
+//		timer = 1.0;
+//		Serial.println(text);
+//		//Serial.print(F(" bytes on pipe "));
+//		//Serial.print(pipe);
+//	}
 }
--- NRF24_Send/NRF24_Send.ino
+++ NRF24_Send/NRF24_Send.ino
@@ -4,17 +4,22 @@
 #include <nRF24L01.h>
 
 RF24 radio(7, 8);
-const byte address[6] = "00101";
+const byte address[][6] = {"1Node","2Node"};
 
 int count = 0;
 
 void setup() {
-	Serial.begin(9600);
-	radio.begin();                  //初始化 nRF24L01摸組
-	radio.openWritingPipe(address); //開始寫入管線
-	radio.setPALevel(RF24_PA_MAX);  //設為低功率
-	radio.stopListening();          //傳送端不需接收
+	Serial.begin(115200);
+	radio.begin();                     //初始化 nRF24L01摸組
+	printf_begin();
+	
+	radio.openWritingPipe(address[1]); //開始寫入管線
+	radio.openReadingPipe(1, address[0]);
+	radio.setPALevel(RF24_PA_MAX);     //設為低功率
 	radio.printDetails();
+	
+	radio.stopListening();             //傳送端不需接收
+	
 }
 
 void loop() {
